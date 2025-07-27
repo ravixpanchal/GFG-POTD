ddf38f4579b09024522fd29f0fc07119d4ff1277
Ravi Panchal <160248997+ravixpanchal@users.noreply.github.com>
2025-07-27 22:48:27 +0530

    Add files via upload
    Signed-off-by: Ravi Panchal  <160248997+ravixpanchal@users.noreply.github.com>

---
+++ Last Moment Before All Ants Fall Out/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int ans=0;
+        
+        for(int i=0;i<left.size();i++){
+            ans=max(ans,left[i]);
+        }
+        
+        for(int j=0;j<right.size();j++){
+            ans=max(ans,n-right[j]);
+        }
+        
+        return ans;
+    }
+};
