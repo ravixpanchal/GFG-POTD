bbaf25d3c0060be5851d3afd3ed2ba2c455c3823
Ravi Panchal <160248997+ravixpanchal@users.noreply.github.com>
2025-07-30 01:38:14 +0530

    Add files via upload
    Signed-off-by: Ravi Panchal  <160248997+ravixpanchal@users.noreply.github.com>

---
+++ Subarrays with sum K/code.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector<int> asciirange(string& s) {
+        vector<int>first(26,-1);
+        vector<int>last(26,-1);
+        vector<int>ans;
+        
+        for(int i=0;i<s.size();i++){
+            int idx=s[i]-'a';
+            if(first[idx]==-1) first[idx]=i; 
+            last[idx]=i;
+        }
+        
+        for(int i=0;i<26;i++){
+            if(first[i]!=-1 && last[i]!=-1 && first[i]<last[i]){
+                int sum=0;
+                for(int j=first[i]+1;j<last[i];j++){
+                    sum+=s[j];
+                }
+                if(sum) ans.push_back(sum);
+            }
+        }
+        return ans;
+        
+    }
+};
