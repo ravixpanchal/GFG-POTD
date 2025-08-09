9d328b071dac5df368ee43bbb8f5afdf7140d6dc
Ravi Panchal <160248997+ravixpanchal@users.noreply.github.com>
2025-08-09 21:08:12 +0530

    Add files via upload
    Signed-off-by: Ravi Panchal  <160248997+ravixpanchal@users.noreply.github.com>

---
+++ Roman Number to Integer/code.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int romanToDecimal(string &s) {
+        // code here
+        unordered_map<char,int>m;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            
+            if(i+1 < s.size() && m[s[i]] < m[s[i+1]]) {
+                ans -= m[s[i]];
+            } else {
+                ans += m[s[i]];
+            }
+        }
+        return ans;
+    }
+};
